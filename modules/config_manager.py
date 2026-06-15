87793907d5c7ae3c037270f5db40d8f496750131
NEANC.or.HZCK <30574631+NEANC@users.noreply.github.com>
2026-06-16 06:48:38 +0800

    chore: 修改默认压缩算法与等级

--- modules/config_manager.py
+++ modules/config_manager.py
@@ -24,8 +24,8 @@ class ConfigManager:
         },
         'zip': {
             'archive_name_format': '存档',
-            'compression_algorithm': 'lzma',
-            'compression_level': '9',
+            'compression_algorithm': 'zstd',
+            'compression_level': '15',
             'archive_mode': 'incremental',
             'max_workers': '1',
         },
@@ -93,8 +93,8 @@ class ConfigManager:
         self.target_folder = ''
         self.archive_folder = ''
         self.archive_name_format = '存档'
-        self.compression_algorithm = 'lzma'
-        self.compression_level = 9
+        self.compression_algorithm = 'zstd'
+        self.compression_level = 15
         self.archive_mode = 'scroll'
         self.max_workers = 1
         self.save_logs = True
@@ -330,8 +330,8 @@ class ConfigManager:
 
         # [zip]
         self.archive_name_format = self._get_str('zip', 'archive_name_format', '存档')
-        self.compression_algorithm = self._get_str('zip', 'compression_algorithm', 'lzma').lower()
-        self.compression_level = self._get_int('zip', 'compression_level', 9)
+        self.compression_algorithm = self._get_str('zip', 'compression_algorithm', 'zstd').lower()
+        self.compression_level = self._get_int('zip', 'compression_level', 15)
         self.archive_mode = self._get_str('zip', 'archive_mode', 'scroll').lower()
         self.max_workers = self._get_int('zip', 'max_workers', 1)
 
